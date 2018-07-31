bbd43470a841f3d5e0c435cbcbc7afaf4ac28749
chriscohoat <chris.cohoat@gmail.com>
2018-07-30 20:02:14 -0600

    Added chown

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import subprocess
 import string
 import random
 import redis
-from os import chmod, getenv, path
+from os import getenv, path, getuid, getgid
 from dotenv import load_dotenv
-from shutil import copy
+from shutil import copy, chown
 
 from cryptography.hazmat.primitives import serialization as crypto_serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
@@ -25,6 +25,9 @@ PATH_TO_EASY_RSA = "/home/ubuntu/EasyRSA-3.0.4/"
 FINISHED_KEY_LOCATION = '/home/ubuntu/client-configs/keys/'
 MAKE_CONFIG_EXECUTABLE = '/home/ubuntu/client-configs/make_config.sh'
 FINAL_OPENVPN_CONFIG_DIRECTORY = '/home/ubuntu/client-configs/files/'
+OPENVPN_CLIENT_CONFIG_DIRECTORY = '/etc/openvpn/ccd/'
+OPENVPN_USER = 'nobody'
+OPENVPN_GROUP = 'nogroup'
 
 app = Flask(__name__)
 redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
@@ -50,32 +53,37 @@ def main():
             return b"Missing `grow_id` in POSTed JSON", 400
         if "type" not in request.json or request.json["type"] not in ALLOWED_TYPES:
             return "Missing `type` in POSTed JSON (must be one of {})".format(", ".join(ALLOWED_TYPES)), 400
-        filename = '{}-{}'.format(request.json["grow_id"], request.json["type"])
-        path_to_full_key = path.join(PATH_TO_EASY_RSA, 'pki/private', '{}.key'.format(filename))
-        path_to_full_cert = path.join(PATH_TO_EASY_RSA, 'pki/issued', '{}.crt'.format(filename))
-        path_to_output_openvpn_config = path.join(FINAL_OPENVPN_CONFIG_DIRECTORY, '{}.ovpn'.format(filename))
+        client_name = '{}-{}'.format(request.json["grow_id"], request.json["type"])
+        path_to_full_key = path.join(PATH_TO_EASY_RSA, 'pki/private', '{}.key'.format(client_name))
+        path_to_full_cert = path.join(PATH_TO_EASY_RSA, 'pki/issued', '{}.crt'.format(client_name))
+        path_to_output_openvpn_config = path.join(FINAL_OPENVPN_CONFIG_DIRECTORY, '{}.ovpn'.format(client_name))
+        path_to_client_config = path.join(OPENVPN_CLIENT_CONFIG_DIRECTORY, client_name)
         if not path.exists(path_to_output_openvpn_config):
             if not path.exists(path_to_full_key):
                 try:
-                    subprocess.Popen(['./easyrsa', 'gen-req', filename, 'nopass', 'batch'], cwd=PATH_TO_EASY_RSA)
+                    subprocess.Popen(['./easyrsa', 'gen-req', client_name, 'nopass', 'batch'], cwd=PATH_TO_EASY_RSA)
                 except Exception as exception:
                     print('Unable to generate key: {}'.format(exception))
                     return b"Failed to generate key", 500
             if not path.exists(path_to_full_cert):
                 try:
-                    subprocess.Popen(['./easyrsa', 'sign-req', 'client', filename, 'batch'], cwd=PATH_TO_EASY_RSA)
+                    subprocess.Popen(['./easyrsa', 'sign-req', 'client', client_name, 'batch'], cwd=PATH_TO_EASY_RSA)
                 except Exception as exception:
                     print('Unable to sign request: {}'.format(exception))
                     return b"Failed to sign request", 500
             copy(path_to_full_key, FINISHED_KEY_LOCATION)
             copy(path_to_full_cert, FINISHED_KEY_LOCATION)
             try:
-                make_config_command = "sudo {} {}".format(MAKE_CONFIG_EXECUTABLE, filename)
+                make_config_command = "sudo {} {}".format(MAKE_CONFIG_EXECUTABLE, client_name)
                 print('Running: {}'.format(make_config_command))
                 subprocess.Popen(make_config_command, shell=True)
             except Exception as exception:
-                    print('Unable to generate final OpenVPN configuration: {}'.format(exception))
-                    return b"Failed to generate configuration", 500
-        with open(path.join(FINAL_OPENVPN_CONFIG_DIRECTORY, '{}.ovpn'.format(filename))) as final_openvpn_config:
+                print('Unable to generate final OpenVPN configuration: {}'.format(exception))
+                return b"Failed to generate configuration", 500
+        if not path.exists(path_to_client_config):
+            with open(path_to_client_config, 'w') as client_config:
+                client_config.write('ifconfig-push 10.1.32.10 10.1.32.1')
+            chown(path_to_client_config, user=OPENVPN_USER, group=OPENVPN_GROUP)
+        with open(path.join(FINAL_OPENVPN_CONFIG_DIRECTORY, '{}.ovpn'.format(client_name))) as final_openvpn_config:
             return jsonify({"config": final_openvpn_config.read()})
     return b"Only POSTing allowed", 405
